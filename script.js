e0bc903b6e814c91c41580606b66e75c9cde3651
KomarovKV <76239374+KomarovKV@users.noreply.github.com>
2021-03-27 23:50:17 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-// $(function () {
-//   $(document).mousemove(function (e) {
-//     $('#cube').css({
-//       '-webkit-transform': 'rotateX(' + e.pageY + 'deg) rotateY(' + e.pageX + 'deg)'
-//     });
-//   });
-// });
 $(function () {
-  $(document).touchmove(function (e) {
+  $(document).mousemove(function (e) {
     $('#cube').css({
       '-webkit-transform': 'rotateX(' + e.pageY + 'deg) rotateY(' + e.pageX + 'deg)'
     });
