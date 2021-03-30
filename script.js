7e2a20a907fcaaba167a01f0936265bb07ea6ed9
KomarovKV <76239374+KomarovKV@users.noreply.github.com>
2021-03-30 19:48:45 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $(document).mousemove(function (e) {
-    $('#cube').css({
-      '-webkit-transform': 'rotateX(' + e.pageY + 'deg) rotateY(' + e.pageX + 'deg)'
-    });
-  });
-});
